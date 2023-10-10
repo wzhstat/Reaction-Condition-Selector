9803c5b83ad00f07bbb6a25acfaa2c10978ae178
wzhstat <139463921+wzhstat@users.noreply.github.com>
2023-10-10 17:48:14 +0800

    Update CountCommonData.py

--- data_processor/CountCommonData.py
+++ data_processor/CountCommonData.py
@@ -5,6 +5,15 @@ csv.field_size_limit(500 * 1024 * 1024)
 
 
 def Cleam_merge(alist):
+    '''
+    Clean and merge a list of dictionaries.
+
+    Args:
+        alist (list): A list of dictionaries.
+
+    Returns:
+        A merged list.
+    '''
     newlist = eval(str(alist))
     outlist = []
     for r in newlist:
@@ -26,10 +35,19 @@ def Cleam_merge(alist):
 def count_data(data_name,save_path = "./data",m1 = 5,mc = 5,ms = 5, mr = 5):
     file_path= "%s/%s_%s+.csv"%(save_path,data_name,m1)
     '''
-    save_path: the floder path to save the data.
-    This function is used to statistics the conditions.
+    Count the common data in a dataset.
+
+    Args:
+        data_name (str): The name of the dataset.
+        save_path (str): The path to save the results.
+        m1 (int): The minimum number of occurrences for a reaction type.
+        mc (int): The minimum number of occurrences for a condition.
+        ms (int): The minimum number of occurrences for a solvent.
+        mr (int): The minimum number of occurrences for a reagent.
+
     '''
     n = 0
+    # Load the data.
     data = pd.read_csv(file_path)
     tem = data['template'][0]
     all_cat_withN = {'None':0}
@@ -39,12 +57,19 @@ def count_data(data_name,save_path = "./data",m1 = 5,mc = 5,ms = 5, mr = 5):
     all_reag_withN = {'None':0}
     all_reag_withoutN = {}
     lens = len(data['template'])
+
+    # Count the common data.
     for i in range(lens):
+        # Print the progress.
         if n% (lens//100) == 0:
             print(n/(lens//100),'%')
+        
+        # Get the data.
         cat = data['cat'][i]
         solv = data['solv'][i]
         reags = [data['reag%s'%j][i] for j in range(4)]
+
+        # Count the common data.
         if cat == 'None':
             all_cat_withN['None'] += 1
         else:
@@ -75,14 +100,13 @@ def count_data(data_name,save_path = "./data",m1 = 5,mc = 5,ms = 5, mr = 5):
                     all_reag_withN[reag] = 1
                     all_reag_withoutN[reag] = 1
 
-        
+    # Sort the results.
     all_cat_withN = {k: v for k, v in sorted(all_cat_withN.items(), key=lambda x: x[1], reverse=True)}
     all_cat_withoutN = {k: v for k, v in sorted(all_cat_withoutN.items(), key=lambda x: x[1], reverse=True)}
     all_solv_withN = {k: v for k, v in sorted(all_solv_withN.items(), key=lambda x: x[1], reverse=True)}
     all_solv_withoutN = {k: v for k, v in sorted(all_solv_withoutN.items(), key=lambda x: x[1], reverse=True)}
     all_reag_withN = {k: v for k, v in sorted(all_reag_withN.items(), key=lambda x: x[1], reverse=True)}
     all_reag_withoutN = {k: v for k, v in sorted(all_reag_withoutN.items(), key=lambda x: x[1], reverse=True)}
-
     all_cat_withN = {k: v for k, v in all_cat_withN.items() if v >= mc}
     all_cat_withoutN = {k: v for k, v in all_cat_withoutN.items() if v >= mc}
     all_solv_withN = {k: v for k, v in all_solv_withN.items() if v >= ms}
@@ -91,7 +115,7 @@ def count_data(data_name,save_path = "./data",m1 = 5,mc = 5,ms = 5, mr = 5):
     all_reag_withoutN = {k: v for k, v in all_reag_withoutN.items() if v >= mr}
 
 
-            
+    # Save the results.    
     with open('%s/all_cat_withN.csv'%save_path,'w', newline='') as catfile:
         writer = csv.writer(catfile)
         writer.writerow(all_cat_withN.keys())
