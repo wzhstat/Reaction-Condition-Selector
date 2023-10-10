f540b0f67a36f479eb06f60ec165c1ef6a1c9fbc
wzhstat <139463921+wzhstat@users.noreply.github.com>
2023-10-10 15:37:05 +0800

    Update __init__.py

--- NnModels/__init__.py
+++ NnModels/__init__.py
@@ -1,4 +1,4 @@
 from .MLPModel import nnModel0, nnModel1, nnModel2
-from .TrainModel import train, get_tem_condition
+from .TrainModel import train, sum_tem_condition
 
-__all__ = ['nnModel0', 'nnModel1', 'nnModel2', 'train','get_tem_condition']
+__all__ = ['nnModel0', 'nnModel1', 'nnModel2', 'train','sum_tem_condition']
