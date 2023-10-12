33cae4bd308281747bdc66e52a406d04307f4d82
wzhstat <139463921+wzhstat@users.noreply.github.com>
2023-10-12 19:26:32 +0800

    Update get_data.py

--- GetMPnnData/get_data.py
+++ GetMPnnData/get_data.py
@@ -9,29 +9,29 @@ from joblib import Parallel, delayed
 import csv
 csv.field_size_limit(500 * 1024 * 1024)
 
-with open('./data/all_cat_withoutN.csv','r') as f:
-    reader = csv.DictReader(f)
-    for classes in reader:
-        cat_list = list(classes.keys())
-        print(len(cat_list))
+def get_condition_list(target,path,withN = False):
+    if withN:
+        N = 'withN'
+    else:
+        N = 'withoutN'
+    if target in ['cat','solv']:
+        with open('%s/all_%s_%s.csv'%(path,target,withN),'r') as f:
+            reader = csv.DictReader(f)
+            for classes in reader:
+                condition_list = list(classes.keys())
+                print('%s num'%target,len(condition_list))
+        return condition_list
 
-with open('./data/all_solv_withoutN.csv','r') as f:
-    reader = csv.DictReader(f)
-    for classes in reader:
-        solv_list = list(classes.keys())
-        print(len(solv_list))
-
-with open('./data/all_reag_withoutN.csv','r') as f:
-    reader = csv.DictReader(f)
-    for classes in reader:
-        reag_list = list(classes.keys())
-        print(len(reag_list))
-
-with open('./data/all_reag_withN.csv','r') as f:
-    reader = csv.DictReader(f)
-    for classes in reader:
-        reag_list1 = list(classes.keys())
-        print(len(reag_list1))
+    if target in ['reag0','reag1','reag2','reag3']:
+        with open('%s/all_reag_%s.csv'%(path,withN),'r') as f:
+            reader = csv.DictReader(f)
+            for classes in reader:
+                reag_list = list(classes.keys())
+                print('reag num',len(reag_list))
+        return reag_list
+    
+    else:
+        raise KeyError("target must be in 'cat,solv,reag0,reag1,reag2,reag3'")
 
 def remove_reagent(Smarts):
     rxn = Smarts.split('>')
@@ -39,24 +39,17 @@ def remove_reagent(Smarts):
     product = rxn[2]
     reactant = reactant.split('.')
     product = product.split('.')
-    outreactant = []
-    for i in reactant:
-        if 'C:' in i or 'CH:' in i or 'CH2:' in i or 'CH3:' in i:
-            outreactant.append(i)
+    outreactant = filter(lambda x: 'C:' in x or 'CH:' in x or 'CH2:' in x or 'CH3:' in x, reactant)
     rout = '.'.join(outreactant)
-    outproduct = []
-    for i in product:
-        if 'C:' in i or 'CH:' in i or 'CH2:' in i or 'CH3:' in i:
-            outproduct.append(i)
+    outproduct = filter(lambda x: 'C:' in x or 'CH:' in x or 'CH2:' in x or 'CH3:' in x, product)
     pout = '.'.join(outproduct)
     out = rout + '>>' + pout
     return out
             
 
-    
-
-
-def get_MPNN_data(target,target_list):
+def save_MPNN_data(target,target_list):
+    data = pd.read_csv('data/1976-2016_5+.csv')
+    rxnsmile = Parallel(n_jobs=-1, verbose=4)(delayed(remove_reagent)(reaction) for reaction in list(data['reaction']))
     MLP_all_data = []
     l = len(rxnsmile)
     for i in range(l):
@@ -68,14 +61,17 @@ def get_MPNN_data(target,target_list):
         dic['smarts'] = rxnsmile[i]
         dic[target] = target_list.index(data[target][i])
         MLP_all_data .append(dic)
-
-
     header = ['smarts',target]
     with open('data/GCN_%s_data_withoutN.csv'%target,'w',newline='') as f:
         f_csv = csv.DictWriter(f,header)
         f_csv.writeheader()
         f_csv.writerows(MLP_all_data)
 
-data = pd.read_csv('data/1976-2016_5+.csv')
-rxnsmile = Parallel(n_jobs=-1, verbose=4)(delayed(remove_reagent)(reaction) for reaction in list(data['reaction']))
-get_MPNN_data('reag1',reag_list)
+def get_MPNN_data(path,withN,target_list):
+    for target in  target_list:
+        print('start to get %s data'%target)
+        get_condition_list(target,path,withN)
+        save_MPNN_data(target,target_list)
+        print('get %s data done'%target)
+        print('------------------')
+   
