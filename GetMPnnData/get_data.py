e459f61b626e1e59668b057a0feafb7fbfb0b6c2
wzhstat <139463921+wzhstat@users.noreply.github.com>
2023-10-23 19:41:57 +0800

    Update get_data.py

--- GetMPnnData/get_data.py
+++ GetMPnnData/get_data.py
@@ -5,36 +5,63 @@ from rdkit.Chem import AllChem
 import rdkit.Chem as Chem
 import numpy as np
 import pandas as pd
+import sys
 from joblib import Parallel, delayed
 import csv
-import sys
+import os
 csv.field_size_limit(500 * 1024 * 1024)
 
-def get_condition_list(target,path,withN = False):
-    if withN:
-        N = 'withN'
-    else:
-        N = 'withoutN'
-    if target in ['cat','solv']:
-        with open('%s/all_%s_%s.csv'%(path,target,withN),'r') as f:
-            reader = csv.DictReader(f)
-            for classes in reader:
-                condition_list = list(classes.keys())
-                print('%s num'%target,len(condition_list))
-        return condition_list
-
-    if target in ['reag0','reag1','reag2','reag3']:
-        with open('%s/all_reag_%s.csv'%(path,withN),'r') as f:
-            reader = csv.DictReader(f)
-            for classes in reader:
-                reag_list = list(classes.keys())
-                print('reag num',len(reag_list))
-        return reag_list
+def open_csv(path:str):
+    '''
+    open csv file and get all cat,solv,reag
+    args:
+        path: csv file path
+    '''
+    with open('%s/all_cat_withoutN.csv'%path,'r') as f:
+        reader = csv.DictReader(f)
+        for classes in reader:
+            cat_list = list(classes.keys())
+            print(len(cat_list))
     
-    else:
-        raise KeyError("target must be in 'cat,solv,reag0,reag1,reag2,reag3'")
+    with open('%s/all_cat_withN.csv'%path,'r') as f:
+        reader = csv.DictReader(f)
+        for classes in reader:
+            cat_list_N = list(classes.keys())
+            print(len(cat_list_N))
+
+    with open('%s/all_solv_withoutN.csv'%path,'r') as f:
+        reader = csv.DictReader(f)
+        for classes in reader:
+            solv_list = list(classes.keys())
+            print(len(solv_list))
+    
+    with open('%s/all_solv_withN.csv'%path,'r') as f:
+        reader = csv.DictReader(f)
+        for classes in reader:
+            solv_list_N = list(classes.keys())
+            print(len(solv_list_N))
+
+    with open('%s/all_reag_withoutN.csv'%path,'r') as f:
+        reader = csv.DictReader(f)
+        for classes in reader:
+            reag_list = list(classes.keys())
+            print(len(reag_list))
+
+    with open('%s/all_reag_withN.csv'%path,'r') as f:
+        reader = csv.DictReader(f)
+        for classes in reader:
+            reag_list_N = list(classes.keys())
+            print(len(reag_list_N))
+    return cat_list,cat_list_N,solv_list,solv_list_N,reag_list,reag_list_N 
+
+
 
 def remove_reagent(Smarts:str):
+    '''
+    remove reagent from reaction
+    args:
+        Smarts: reaction smarts
+    '''
     rxn = Smarts.split('>')
     reactant = rxn[0]
     product = rxn[2]
@@ -48,10 +75,25 @@ def remove_reagent(Smarts:str):
     return out
             
 
-def open_MPNN_data(path: str,file_name: str,target: str,target_list: list, data: pd.DataFrame):
+
+def open_MPNN_data(path: str,file_name: str,target: str,target_list: list, data: pd.DataFrame,condition: list, cat_list,solv_list,reag_list):
+    '''
+    open csv file and get all data for MPNN
+    args:
+        path: csv file path
+        file_name: csv file name
+        target: target name
+        target_list: target list
+        data: csv file data
+        condition: condition that is used for MPNN
+        cat_list: cat list
+        solv_list: solv list
+        reag_list: reag list
+    '''
     data = data
     rxnsmile = Parallel(n_jobs=-1, verbose=4)(delayed(remove_reagent)(reaction) for reaction in list(data['reaction']))
     MLP_all_data = []
+    all_condition_list = []
     l = len(rxnsmile)
     for i in range(l):
         if i % (l//100) == 0:
@@ -59,50 +101,96 @@ def open_MPNN_data(path: str,file_name: str,target: str,target_list: list, data:
             print("Progress: {}%: ".format(i/(l//100)), "▋" * int(i/(l//50)), end="")
             sys.stdout.flush()
         dic = {}
-        if data[target][i] not in target_list:
+        if data[target][i] is np.nan:
+            titem = 'None'
+        else:
+            titem = data[target][i]
+        if titem not in target_list:
             continue
         dic['smarts'] = rxnsmile[i]
-        dic[target] = target_list.index(data[target][i])
+        dic[target] = target_list.index(titem)
+        condition_list = []
+        if condition != None:
+            for conditions in condition:
+                get_one_hot = np.zeros(len(eval('%s_list'%conditions)))
+                if data[conditions][i] is np.nan:
+                    citem = 'None'
+                else:
+                    citem = data[conditions][i]
+                if citem in eval('%s_list'%conditions):
+                    one_hot_index = eval('%s_list'%conditions).index(citem)
+                    get_one_hot[one_hot_index] = 1
+                condition_list.append(get_one_hot)
+            if len(condition_list) == 0:
+                continue
+            if len(condition_list) > 1:
+                ohc = np.concatenate(condition_list)
+            else:
+                ohc = condition_list[0]
+            all_condition_list.append(ohc)
         MLP_all_data.append(dic)
-    return MLP_all_data
+    condition_np = np.array(all_condition_list)
+    return MLP_all_data,condition_np
 
-def add_condition(data , condition:str, MLP_all_data: [dict]):
-    l = len(MLP_all_data)
-    for i in range(len(MLP_all_data)):
-        if i % (l//100) == 0:
-            print("\r", end="")
-            print("Progress: {}%: ".format(i/(l//100)), "" * int(i/(l//50)), end="")
-            sys.stdout.flush()
-        MLP_all_data[i][condition] = data[condition][i]
-    return MLP_all_data
-
-def save_MPNN_csv(path,condition,MLP_all_data,target,N):
+def save_MPNN_csv(path,condition,MLP_all_data,condition_np,target,N):
+    '''
+    save data to csv file
+    args:
+        path: save path
+        condition: condition that is used for MPNN
+        MLP_all_data: all data
+        condition_np: condition data
+        target: target name
+        N: whether to use N
+    '''
     if condition == None:
         header = ['smarts',target]
     else:
-        header = ['smarts',condition,target]
+        header = ['smarts',target]
+        condition_header = [','.join(condition)]
     if N:
-        path = '%s/GCN_%s_data_withN.csv'%(path,target)
+        path = '%s/GCN_%s_data_withN'%(path,target)
     else:
-        path = '%s/GCN_%s_data_withoutN.csv'%(path,target)
-    with open(path,'w',newline='') as f:
-        f_csv = csv.DictWriter(f,header)
+        path = '%s/GCN_%s_data_withoutN'%(path,target)
+    os.mkdir(path)
+    with open(path+"/GCN_data.csv",'w',newline='') as f:
+        f_csv = csv.DictWriter(f,header,extrasaction='ignore')
         f_csv.writeheader()
         f_csv.writerows(MLP_all_data)
+    if condition != None:
+        np.save(path+"/condition.npy",condition_np)
 
-def get_GCNN_data(path: str, file_name: str, target: str, conditon: str, N: bool = False):
+def get_GCNN_data(path: str, file_name: str, target: str, conditons: list, N: bool = False):
+    '''
+    get data for GCNN
+    args:
+        path: csv file path
+        file_name: csv file name
+        target: target name
+        conditons: condition that is used for MPNN
+        N: whether to use N
+    '''
     data = pd.read_csv('%s/%s.csv'%(path,file_name))
-    target_list = get_condition_list(target,path,withN = N)
-    MLP_all_data = open_MPNN_data(path,file_name,target,target_list,data)
-    if conditon != None:
-        MLP_all_data = add_condition(data,conditon,MLP_all_data)
-    save_MPNN_csv(path,conditon,MLP_all_data,target,N)
+    cat_list,cat_list_N,solv_list,solv_list_N,reag_list,reag_list_N = open_csv(path)
+    if N:
+        if target in ['cat','solv']:
+            target_list = eval('%s_list_N'%target)
+        else:
+            target_list = reag_list_N
+        data,cat_list,solv_list,reag_list = data,cat_list_N,solv_list_N,reag_list_N
+    else:
+        if target in ['cat','solv']:
+            target_list = eval('%s_list'%target)
+        else:
+            target_list = reag_list
+        data,cat_list,solv_list,reag_list = data,cat_list,solv_list,reag_list
+    MLP_all_data,condition_np = open_MPNN_data(path,file_name,target,target_list,data,conditons,cat_list,solv_list,reag_list)
+    save_MPNN_csv(path,conditons,MLP_all_data,condition_np,target,N)
 
 if __name__ == '__main__':
     get_GCNN_data('./data','1976-2016_5+','cat',None,N=True)
-    get_GCNN_data('./data','1976-2016_5+','solv','cat',N=True)
-    get_GCNN_data('./data','1976-2016_5+','reag0','solv',N=True)
-    get_GCNN_data('./data','1976-2016_5+','cat',None)
-    get_GCNN_data('./data','1976-2016_5+','solv','cat')
-    get_GCNN_data('./data','1976-2016_5+','reag0','solv')
-   
+    get_GCNN_data('./data','1976-2016_5+','solv',['cat'],N=True)
+    get_GCNN_data('./data','1976-2016_5+','reag0',['cat','solv'],N=True)
+
+
+    
