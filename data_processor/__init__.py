4af30bbe2f2458936f835b1bb76c640f2847d411
wzhstat <139463921+wzhstat@users.noreply.github.com>
2024-01-27 22:25:35 +0800

    Delete data_processor directory

--- data_processor/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ExtractData import get_datas, get_m_data
-from .GetTem import get_temp, classif_by_temp
-from .CountCommonData import count_data
-
-__all__ = ['get_datas', 'get_5_data', 'get_temp', 'classif_by_temp', 'count_data']
