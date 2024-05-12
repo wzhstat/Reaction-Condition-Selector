f0cca634fa5b204eb0f52defe03e733b238f6f07
wzhstat <139463921+wzhstat@users.noreply.github.com>
2024-05-12 15:58:38 +0800

    Add files via upload

---
+++ make_sys_predictions.sh
@@ -0,0 +1,2 @@
+python prediction.py --test_path ./data/sys_data.csv --save_path ./data/sys_prediction
+python cluster_prediction.py --test_path ./data/sys_data.csv --save_path ./data/sys_prediction
