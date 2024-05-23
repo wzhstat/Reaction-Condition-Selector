3f23ffdc2d09424783610cf0342402231dcfaeca
Zihan, Wang <139463921+wzhstat@users.noreply.github.com>
2024-05-23 23:36:43 +0800

    Delete DataProcessor directory

--- DataProcessor/__init__.py
+++
@@ -1,5 +0,0 @@
-from .extract_data import get_datas, get_m_data, split_data
-from .get_tem import get_temp, classif_by_temp
-from .count_common_data import count_data
-
-__all__ = ['get_datas', 'get_temp', 'classif_by_temp', 'count_data', 'get_m_data', 'split_data']
