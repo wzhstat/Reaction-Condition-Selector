29b20516fdadb77efc2984001c6119a5d7c175d2
Zihan, Wang <139463921+wzhstat@users.noreply.github.com>
2024-05-25 18:12:20 +0800

    Update and rename make_sys_predictions.sh to make_JMC_predictions.sh

--- make_sys_predictions.sh
+++
@@ -1,2 +0,0 @@
-python prediction.py --test_path ./data/sys_data.csv --save_path ./data/sys_prediction
-python cluster_prediction.py --test_path ./data/sys_data.csv --save_path ./data/sys_prediction
